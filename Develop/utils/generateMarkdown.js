683958acf57c180fc52b9734636ca0a10c06c3ed
Nick Reisner <nreisne@wgu.edu>
2021-08-21 09:38:39 -0500

    adding to markdown

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -4,14 +4,21 @@ const license = {
 
 // If there is no license, return an empty string
 function renderLicenseBadge(license) {
-  if (license === "MIT") {
+  if (license === "None") {
+    return (
+      `![License: MIT](https://img.shields.io/badge/license-${license}-blue.svg)](https://opensource.org/licenses/${license}`
+    )
     return ''
   }
 }
 
 // If there is no license, return an empty string
 function renderLicenseLink(license) {
-  
+  if (license!== "None") {
+    return {
+      
+    }
+  }
 }
 
 
