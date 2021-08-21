54138e51d81d5241a33bc2531c9653b341134f64
Nick Reisner <nreisne@wgu.edu>
2021-08-21 09:34:36 -0500

    adding to general markdown

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,20 +1,28 @@
 const license = {
-if(license === MIT)
-then 
+  "MIT": "[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)",
 }
+
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  if (license === "MIT") {
+    return ''
+  }
+}
+
+// If there is no license, return an empty string
+function renderLicenseLink(license) {
+  
+}
 
 
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseSection(license) {
 
-// If there is no license, return an empty string
-function renderLicenseSection(license) {}
+}
 
 function generateMarkdown(data) {
   return `# ${data.title}
-
+  ${license[data.license] || ""}
 `;
 }
 
